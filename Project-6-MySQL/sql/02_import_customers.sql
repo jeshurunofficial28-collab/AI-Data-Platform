7303610c4b27e1d1e473815a5d96e11eeda8ca50
Jeshurun Solly <jeshurunsolly28@mail.com>
2026-07-20 16:29:38 +0530

    Completed Project 6 - MySQL Business Analysis

---
+++ Project-6-MySQL/sql/02_import_customers.sql
@@ -0,0 +1,19 @@
+-- ==================================================
+-- Project 6 - MySQL
+-- Lesson 2 : Import Customers Dataset
+-- ==================================================
+
+-- Objective:
+-- Import the Customers dataset into the ai_data_platform database.
+
+USE ai_data_platform;
+
+-- Dataset:
+-- customers.csv
+
+-- Table Name:
+-- customers
+
+-- Note:
+-- The Customers dataset will be imported using the
+-- MySQL Workbench Table Data Import Wizard.
